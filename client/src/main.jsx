7fc4e58d25a4253c804210dbf0fa075cea939b5d
Mathiloc <you@example.com>
2025-11-01 23:37:51 -0500

    Modificaciones al main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// src/main.jsx
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { GlobalProvider } from './context/GlobalContext'; // Importar el proveedor de contexto
+import './index.css'; // Estilos globales
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* Envolvemos toda la aplicación con GlobalProvider para que 
+        todas las páginas tengan acceso al estado de la sesión y la navegación. */}
+    <GlobalProvider>
+      <App />
+    </GlobalProvider>
+  </React.StrictMode>,
+);
